53de40fcd559b98fc4afec2ccfd28f04444e55bc
Michael Cramer <cramer@webkist.com>
2015-02-21 16:20:26 -0500

    much closer to working
    get some proper 3d math and simpler rotation effects.

--- NeoCompass.ino
+++ NeoCompass.ino
@@ -6,11 +6,14 @@
 // NeoPixel 12-LED ring on PIN 12
 #define LEDS 12
 #define PIN 12
+#define PIXEL_SHIFT 40
+uint32_t topColor, bottomColor;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 // Compass+Accellerometer
-Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
-
+//Adafruit_LSM303_Accel_Unified sensor = Adafruit_LSM303_Accel_Unified(12345);
+Adafruit_LSM303_Mag_Unified sensor = Adafruit_LSM303_Mag_Unified(12345);
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -18,14 +21,19 @@ void setup() {
   strip.begin();
   strip.clear();
   strip.show();  
+
+  topColor = strip.Color(1 << 3, 0, 0);
+  bottomColor = strip.Color(0, 0, 1 << 3);
+
   Serial.begin(9600);
 
-  if(!mag.begin())
+  if(!sensor.begin())
   {
     /* There was a problem detecting the compass ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
+sensor.enableAutoRange(true);
 
 }
 
@@ -33,40 +41,81 @@ int zval = 0;
 sensors_event_t event; 
 float Pi = 3.14159;
 float heading;
+float headingz;
+float xyval;
 int pixel;
+float x, y, z, gmag, zpercent, topLEDs;
 
 // the loop routine runs over and over again forever:
 void loop() {
-  mag.getEvent(&event);
-
-  heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+  sensor.getEvent(&event);
+  x = event.acceleration.x;
+  y = event.acceleration.y;
+  z = event.acceleration.z;
+  //x = event.magnetic.x;
+  //y = event.magnetic.y;
+  //z = event.magnetic.z;
+  gmag = sqrt(sq(x) + sq(y) + sq(z));
+  zpercent = (z / gmag + 1.0) / 2.0;
+  
+  xyval = sqrt(sq(z) + sq(y));
+  heading   = (atan2(x, y) * 180) / Pi;
+  headingz = (atan2(z, xyval) * 180) / Pi;
+  
+  Serial.print("x: "); Serial.print(x); 
+  Serial.print(" y: "); Serial.print(y); 
+  Serial.print(" z: "); Serial.print(z); 
+  Serial.print(" z%: "); Serial.println(zpercent); 
 
   // Normalize to 0-360
-  heading = heading + (heading < 0 ? 360 : 0);
-
-  pixel = ((int) ((heading / 30) + 8)) % 12;
-  Serial.print("heading: "); 
-  Serial.print((int) heading); 
-  Serial.print(" [");
-  Serial.print(pixel); 
-  Serial.println("]");
+  heading   = heading + 360 + PIXEL_SHIFT;
+  if(heading >= 360.0) { heading = heading - 360.0; }
+  pixel = ((int) (heading / 30)) % 12;
 
   strip.clear();
-  // The north-pointing pixel.
-  strip.setPixelColor(pixel, 1 << 7, 0, 0);
-  for(int i=0; i<4; i++) {
-    // A bit to the west...
-    strip.setPixelColor((pixel + 8 + i) % 12, 1 << (i + 2), 0, 0);
-    // A bit to the east...
-    strip.setPixelColor((pixel + (4 - i)) % 12, 1 << (i + 2  ), 0, 0);
+
+  // The north- & south-pointing pixels.
+  strip.setPixelColor(pixel, topColor);
+  // strip.setPixelColor((pixel + 6) % 12, bottomColor);
+         if(zpercent < 0.1) {
+    topLEDs = 0;
+  } else if(zpercent < 0.2) {
+    topLEDs = 1;
+  } else if(zpercent < 0.3) {
+    topLEDs = 2;
+  } else if(zpercent < 0.4) {
+    topLEDs = 3;
+  } else if(zpercent < 0.5) {
+    topLEDs = 3;
+  } else if(zpercent < 0.6) {
+    topLEDs = 3;
+  } else if(zpercent < 0.7) {
+    topLEDs = 4;
+  } else if(zpercent < 0.8) {
+    topLEDs = 5;
+  } else if(zpercent < 0.9) {
+    topLEDs = 6;
+  } else {
+    topLEDs = 7;
   }
+    
+  // topLEDs = (int) (zpercent * 6);
+  for(int i=0; i<topLEDs; i++) {
+    // A bit to the west...
+    strip.setPixelColor((pixel + i) % 12, topColor);
+    // strip.setPixelColor((pixel + 6 + i) % 12, 0, 0, bottomColor);
+    // A bit to the east...
+    strip.setPixelColor((pixel + 12 - i) % 12, topColor);
+    // strip.setPixelColor((pixel + 6 - i) % 12, bottomColor);
+  }
+  for(int i=topLEDs; i<7; i++) {
+    // A bit to the west...
+    strip.setPixelColor((pixel + i) % 12, bottomColor);
+    // A bit to the east...
+    strip.setPixelColor((pixel + 12 - i) % 12, bottomColor);
+  }
+  
   strip.show();
   delay(100);
 }
-
-
-
-
-
-
 
